79aa5b4940c73b2542dc23dc5dcd97096cb41d02
asmanp2012 <asmanp2012@gmail.com>
2022-04-07 10:46:08 +0430

    feat(demo/guitar): playMulti, GuitarPlayOption

--- demo/src/guitar/test.ts
+++ demo/src/guitar/test.ts
@@ -4,12 +4,26 @@ import { Guitar, achordList } from '@music-sizer/guitar';
 const myMusic = new Music();
 const guitar = new Guitar('guitar', myMusic);
 
+myMusic.timeSignature = {
+  numerator: 3,
+  denominator: 4
+};
+
 guitar.pic(achordList.Am);
-guitar.playSuccessive([1, 2, 3, 4, 5, 6]);
+guitar.playSuccessive({ wireList: [1, 2, 3, 4, 5, 6] });
 
 guitar.next(1);
 
 guitar.pic(achordList.Em);
-guitar.playSuccessive([1, 2, 3, 4, 5, 6]);
+guitar.playSuccessive({ wireList: [1, 2, 3, 4, 5, 6] });
+
+guitar.next(1);
+
+guitar.moveBar(2);
+guitar.playMulti({ wireList: [1, 2, 3, 4, 5, 6] });
+
+guitar.next(1);
+guitar.pic(achordList.Am);
+guitar.playMulti({ wireList: [1, 2, 3, 4, 5, 6] });
 
 myMusic.save('./demo/guitar/test.mid');
