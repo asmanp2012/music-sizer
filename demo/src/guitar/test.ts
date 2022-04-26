e7a545989e56e5cee090d5575f93648e6c14ee58
Mohammad Khodadadi <asmanp2012@gmail.com>
2022-04-26 17:50:51 +0430

    refactor(demo/guitar): test playMulti

--- demo/src/guitar/test.ts
+++ demo/src/guitar/test.ts
@@ -7,12 +7,16 @@ const guitar = new Guitar('guitar', myMusic);
 myMusic.tempo = 60;
 
 myMusic.timeSignature = {
-  numerator: 3,
+  numerator: 4,
   denominator: 4
 };
 
 guitar.pic(achordList.Am);
-guitar.playSuccessive({ wireList: [1, 2, 3, 4, 5, 6] });
+guitar.playMulti({
+  wireList: [6, 5, 4, 3, 2, 1],
+  inputDuration: { type: 3, length: 1 },
+  distancePerNote: { type: 4, length: 1 }
+});
 
 guitar.next(1);
 
@@ -20,12 +24,5 @@ guitar.pic(achordList.Em);
 guitar.playSuccessive({ wireList: [1, 2, 3, 4, 5, 6] });
 
 guitar.next(1);
-
-guitar.moveBar(2);
-guitar.playMulti({ wireList: [1, 2, 3, 4, 5, 6] });
-
-guitar.next(1);
-guitar.pic(achordList.Am);
-guitar.playMulti({ wireList: [1, 2, 3, 4, 5, 6] });
 
 myMusic.save('./demo/guitar/test.mid');
