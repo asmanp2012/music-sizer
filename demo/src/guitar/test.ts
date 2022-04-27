cb94275565831585ded3c6eb2b18080fe67a3585
Mohammad Khodadadi <asmanp2012@gmail.com>
2022-04-27 15:14:19 +0430

    refactor(demo/guitar): test 4/2 rythem

--- demo/src/guitar/test.ts
+++ demo/src/guitar/test.ts
@@ -4,7 +4,7 @@ import { Guitar, achordList } from '@music-sizer/guitar';
 const myMusic = new Music();
 const guitar = new Guitar('guitar', myMusic);
 
-myMusic.tempo = 60;
+myMusic.tempo = 120;
 
 myMusic.timeSignature = {
   numerator: 4,
@@ -15,13 +15,21 @@ guitar.pic(achordList.Am);
 guitar.playMulti({
   wireList: [6, 5, 4, 3, 2, 1],
   inputDuration: { type: 3, length: 1 },
-  distancePerNote: { type: 4, length: 1 }
+  distancePerNote: { type: 7, length: 1 }
 });
 
 guitar.next(1);
 
-guitar.pic(achordList.Em);
-guitar.playSuccessive({ wireList: [1, 2, 3, 4, 5, 6] });
+guitar.playMulti({
+  wireList: [1, 2, 3, 4, 5, 6],
+  inputDuration: { type: 4, length: 1 },
+  distancePerNote: { type: 8, length: 1 }
+});
+guitar.delay({ type: 4, length: 1 });
+myMusic.play({
+  instrument: 'guitar',
+  noteList: ['C#6']
+});
 
 guitar.next(1);
 
