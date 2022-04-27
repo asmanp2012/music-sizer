d0497e0b45af902f6cedf3b139be6d19ec82abf4
asmanp2012 <asmanp2012@gmail.com>
2022-04-28 03:16:42 +0430

    feat(demo/guitar): complite test 2/4 time timeSignature

--- demo/src/guitar/test.ts
+++ demo/src/guitar/test.ts
@@ -4,33 +4,43 @@ import { Guitar, achordList } from '@music-sizer/guitar';
 const myMusic = new Music();
 const guitar = new Guitar('guitar', myMusic);
 
-myMusic.tempo = 120;
+myMusic.tempo = 25;
 
 myMusic.timeSignature = {
   numerator: 4,
   denominator: 4
 };
 
-guitar.pic(achordList.Am);
+guitar.pic(achordList.Em);
 guitar.playMulti({
   wireList: [6, 5, 4, 3, 2, 1],
   inputDuration: { type: 3, length: 1 },
   distancePerNote: { type: 7, length: 1 }
-});
+}, true);
 
 guitar.next(1);
 
+guitar.pic(achordList.Am);
 guitar.playMulti({
   wireList: [1, 2, 3, 4, 5, 6],
   inputDuration: { type: 4, length: 1 },
   distancePerNote: { type: 8, length: 1 }
-});
-guitar.delay({ type: 4, length: 1 });
-myMusic.play({
+}, true);
+
+myMusic.playSuccessive({
   instrument: 'guitar',
-  noteList: ['C#6']
+  noteList: [null, 'C#6'],
+  inputDuration: { type: 4, length: 1 },
+  velocity: 120
 });
 
-guitar.next(1);
+myMusic.delay('guitar', { type: 4, length: 1 });
+
+guitar.pic(achordList.Em);
+guitar.playMulti({
+  wireList: [1, 2, 3, 4, 5, 6],
+  inputDuration: { type: 4, length: 1 },
+  distancePerNote: { type: 8, length: 1 }
+}, true);
 
 myMusic.save('./demo/guitar/test.mid');
