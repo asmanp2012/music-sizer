0763599755adef06b41a75f1f0188a61a4c22403
Godgiven <60387129+asmanp2012@users.noreply.github.com>
2026-05-26 21:36:22 +0330

    refactor(build): add ESLint configuration and update package scripts and dependencies and migrate to bun
    - Created a new ESLint configuration file with TypeScript and recommended settings.
    - Updated package scripts to use Bun instead of Yarn for build, watch, and serve commands.
    - Updated dependencies to their latest versions, including ESLint and TypeScript related packages.

---
+++ eslint.config.js
@@ -0,0 +1,83 @@
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import eslintPluginWc from "eslint-plugin-wc";
+import eslintPluginJsdoc from "eslint-plugin-jsdoc";
+import globals from "globals";
+
+export default tseslint.config(
+  {
+    ignores: [
+      "node_modules",
+      "dist",
+      "*.js",
+      "*.map",
+      "*.d.ts",
+    ]
+  },
+  {
+    languageOptions: {
+      ecmaVersion: 12,
+      sourceType: "module",
+      globals: {
+        ...globals.browser,
+        ...globals.es2021
+      }
+    }
+  },
+
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  eslintPluginWc.configs["flat/recommended"],
+  eslintPluginJsdoc.configs["flat/recommended"],
+
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        project: "./ts-lint.json",
+        tsconfigRootDir: import.meta.dirname,
+      }
+    },
+    plugins: {
+      "wc": eslintPluginWc,
+      "jsdoc": eslintPluginJsdoc
+    },
+    settings: {
+      wc: {
+        elementBaseClasses: ["LitElement"]
+      }
+    },
+    rules: {
+      "no-eval": ["error", { allowIndirect: true }],
+      "curly": ["error", "all"],
+      "comma-dangle": ["error", {
+        arrays: "only-multiline",
+        objects: "only-multiline",
+        imports: "only-multiline",
+        exports: "only-multiline",
+        functions: "only-multiline"
+      }],
+      "no-void": ["error", { allowAsStatement: true }],
+      "@typescript-eslint/return-await": ["error", "in-try-catch"],
+      "@typescript-eslint/no-inferrable-types": ["off", {
+        ignoreParameters: true,
+        ignoreProperties: true
+      }],
+      "@typescript-eslint/no-misused-promises": ["error", {
+        checksVoidReturn: false,
+        checksConditionals: true
+      }],
+
+      "import/export": "off",
+      "jsdoc/require-param": "off",
+      "jsdoc/require-returns": "off",
+      "@typescript-eslint/no-dynamic-delete": "off",
+      "@typescript-eslint/prefer-includes": "off",
+      "@typescript-eslint/no-invalid-void-type": "off",
+      "@typescript-eslint/no-non-null-assertion": "off",
+      "@typescript-eslint/promise-function-async": "off",
+      "@typescript-eslint/no-unnecessary-boolean-literal-compare": "off"
+    }
+  }
+);
